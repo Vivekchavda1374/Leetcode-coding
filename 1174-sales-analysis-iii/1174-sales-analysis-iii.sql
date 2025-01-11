612dacf2968fe65ad60265dfb823940648dc5633
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-01-11 12:13:50 +0530

    Time: 2138 ms (18.42%), Space: 0B (100%) - LeetHub

--- 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
@@ -1,5 +1,6 @@
-SELECT Product.product_id, Product.product_name FROM Product 
-JOIN Sales 
-ON Product.product_id = Sales.product_id 
-GROUP BY Sales.product_id 
-HAVING MIN(Sales.sale_date) >= "2019-01-01" AND MAX(Sales.sale_date) <= "2019-03-31";
+select p.product_id , p.product_name from Product p
+inner join 
+Sales s on 
+p.product_id = s.product_id
+group by p.product_id , p.product_name
+having min(s.sale_date) >= '2019-01-01' and  max(s.sale_date) <= '2019-03-31';
