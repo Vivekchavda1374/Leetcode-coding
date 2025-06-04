6ab8acebe6508de985586f52ad8306c685dd972d
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-06-04 21:02:02 +0530

    Time: 0 ms (100%), Space: 40.8 MB (15.05%) - LeetHub

---
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int sign = 1;
+        int sum = 0 ;
+        while(n>0){
+           int rem =n%10;
+           sign*=-1;
+           sum += sign*rem;
+           n /= 10;
+            
+        }
+        return sign * sum;
+    }
+}
