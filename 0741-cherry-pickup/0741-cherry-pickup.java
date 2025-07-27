0e2460225e1d614f116b3a5d79c2c3efe6cccfe0
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-07-27 23:16:07 +0530

    Time: 13 ms (96.97%), Space: 44.8 MB (77.47%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,36 +1,45 @@
 class Solution {
+    int[][] grid;
+    int n;
+    Integer[][][] memo;
+
     public int cherryPickup(int[][] grid) {
-        int n = grid.length;
-        int[][][] dp = new int[2 * n - 1][n][n];
-        for (int[][] layer : dp)
-            for (int[] row : layer)
-                Arrays.fill(row, Integer.MIN_VALUE);
+        this.grid = grid;
+        this.n = grid.length;
+        this.memo = new Integer[n][n][n];
+        int res = dfs(0, 0, 0);
+        return Math.max(0, res);
+    }
 
-        dp[0][0][0] = grid[0][0];
+    private int dfs(int r1, int c1, int r2) {
+        int c2 = r1 + c1 - r2;
+        if (r1 >= n || c1 >= n || r2 >= n || c2 >= n ||
+            grid[r1][c1] == -1 || grid[r2][c2] == -1)
+            return Integer.MIN_VALUE;
+        if (r1 == n - 1 && c1 == n - 1)
+            return grid[r1][c1];
 
-        for (int k = 1; k < 2 * n - 1; k++) {
-            for (int i = Math.max(0, k - n + 1); i <= Math.min(n - 1, k); i++) {
-                for (int j = Math.max(0, k - n + 1); j <= Math.min(n - 1, k); j++) {
-                    if (grid[i][k - i] == -1 || grid[j][k - j] == -1)
-                        continue;
+        if (memo[r1][c1][r2] != null)
+            return memo[r1][c1][r2];
 
-                    int max = Integer.MIN_VALUE;
-
-                    for (int pi = i - 1; pi <= i; pi++) {
-                        for (int pj = j - 1; pj <= j; pj++) {
-                            if (pi >= 0 && pj >= 0)
-                                max = Math.max(max, dp[k - 1][pi][pj]);
-                        }
-                    }
-
-                    if (max == Integer.MIN_VALUE) continue;
-
-                    int curr = (i == j) ? grid[i][k - i] : grid[i][k - i] + grid[j][k - j];
-                    dp[k][i][j] = max + curr;
-                }
-            }
+        int cherries = grid[r1][c1];
+        if (r1 != r2 || c1 != c2) {
+            cherries += grid[r2][c2];
         }
 
-        return Math.max(0, dp[2 * n - 2][n - 1][n - 1]);
+        int max = Integer.MIN_VALUE;
+
+        max = Math.max(max, dfs(r1 + 1, c1, r2 + 1)); 
+        max = Math.max(max, dfs(r1 + 1, c1, r2));     
+        max = Math.max(max, dfs(r1, c1 + 1, r2 + 1)); 
+        max = Math.max(max, dfs(r1, c1 + 1, r2));    
+
+        if (max == Integer.MIN_VALUE) {
+            memo[r1][c1][r2] = Integer.MIN_VALUE;
+            return Integer.MIN_VALUE;
+        }
+
+        memo[r1][c1][r2] = cherries + max;
+        return memo[r1][c1][r2];
     }
 }
