5fe7f89efd4cddbb770fd7b63cb2e1030672677b
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-07-28 07:46:16 +0530

    Time: 0 ms (100%), Space: 41.3 MB (98.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
 
         int[][] dp = new int[m][n];
         for (int[] row : dp) {
@@ -13,8 +13,7 @@ class Solution {
     }
 
     private int countPaths(int i, int j, int[][] dp, int[][] grid) {
-        if (i < 0 || j < 0) return 0;
-        if (grid[i][j] == 1) return 0; 
+        if (i < 0 || j < 0 || grid[i][j] == 1) return 0;
         if (i == 0 && j == 0) return 1;
 
         if (dp[i][j] != -1) return dp[i][j];
