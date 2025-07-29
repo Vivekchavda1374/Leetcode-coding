e1066adb36cfaa08f76ca8cd078164a6357685dd
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-07-29 10:02:42 +0530

    Time: 0 ms (100%), Space: 41.3 MB (34.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,21 @@
 class Solution {
+    int[] dp;
+
     public int rob(int[] nums) {
-      int rob1=0;
-      int rob2 =0;
-      int max=0;
-      for(int i =0;i<nums.length;i++){
-          max=Math.max(rob1+nums[i],rob2); 
-          rob1=rob2;
-          rob2=max;
-      }  
-      return max;    
+        int n = nums.length;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        return robHelper(nums, n - 1);
+    }
+
+    private int robHelper(int[] nums, int i) {
+        if (i < 0) return 0;
+        if (dp[i] != -1) return dp[i];
+
+        int include = robHelper(nums, i - 2) + nums[i];
+        int exclude = robHelper(nums, i - 1);
+
+        dp[i] = Math.max(include, exclude);
+        return dp[i];
     }
 }
