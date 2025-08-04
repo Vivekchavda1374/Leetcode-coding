902c10748ce69c4aa732d90810c58cbb7c8f30fe
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-08-04 08:17:45 +0530

    Time: 40 ms (69.36%), Space: 43 MB (20.79%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,32 +1,25 @@
 class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
-        int[] arr = new int[n+2];
+        int[] arr = new int[n + 2];
         arr[0] = 1;
-        arr[n+1] = 1;
-        for(int i = 0; i<n; i++){
-            arr[i+1] = nums[i];
+        arr[n + 1] = 1;
+        for (int i = 0; i < n; i++) {
+            arr[i + 1] = nums[i];
         }
-        int[][] dp = new int[n+2][n+2];
-        for(int [] d : dp){
-            Arrays.fill(d,-1);
+
+        int[][] dp = new int[n + 2][n + 2];
+
+        for (int len = 1; len <= n; len++) {
+            for (int left = 1; left <= n - len + 1; left++) {
+                int right = left + len - 1;
+                for (int k = left; k <= right; k++) {
+                    dp[left][right] = Math.max(dp[left][right],
+                        dp[left][k - 1] + arr[left - 1] * arr[k] * arr[right + 1] + dp[k + 1][right]);
+                }
+            }
         }
-         return burstballon_rec(arr , 0, n+1,dp);
-        
-    }
-    public int burstballon_rec(int[] arr , int s , int e, int[][] dp){
-        if(s+1 >= e) return 0;
-        if(dp[s][e] != -1) return dp[s][e];
-        int ans = Integer.MIN_VALUE;
-        for(int i = s+1; i<e; i++){
-            int leftans = burstballon_rec(arr , s , i,dp);
-            int rightans = burstballon_rec(arr , i , e,dp);
-            int myans = leftans+rightans + arr[s]*arr[i]*arr[e];
-           ans = Math.max(ans , myans);
-        }
-        dp[s][e] = ans;
-        return ans;
-        
+
+        return dp[1][n];
     }
 }
-
