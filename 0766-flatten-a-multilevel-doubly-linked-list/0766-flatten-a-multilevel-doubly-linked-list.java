831e77cb35e283a7ccd845d4c7d97cb2a8e6b00a
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-11-10 11:05:42 +0530

    Time: 0 ms (100%), Space: 43.7 MB (6.43%) - LeetHub

---
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,39 @@
+// Definition for a Node.
+// class Node {
+//     public int val;
+//     public Node prev;
+//     public Node next;
+//     public Node child;
+// }
+
+class Solution {
+    public Node flatten(Node head) {
+        if (head == null) return null;
+        DFS(head);
+        return head;
+    }
+
+    private Node DFS(Node node) {
+        Node curr = node;
+        Node last = null;
+
+        while (curr != null) {
+            Node next = curr.next;
+            if (curr.child != null) {
+                Node childTail = DFS(curr.child);
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                if (next != null) {
+                    childTail.next = next;
+                    next.prev = childTail;
+                }
+                curr.child = null;
+                last = childTail;
+            } else {
+                last = curr;
+            }
+            curr = next;
+        }
+        return last;
+    }
+}
